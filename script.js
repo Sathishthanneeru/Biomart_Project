f45986b5d083ec745bfec732236b03a39c8f26b8
Sathishthanneeru <sathishthanneeru36@gmail.com>
2025-08-05 14:17:30 +0530

    ui done

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $('.video-carousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+});
